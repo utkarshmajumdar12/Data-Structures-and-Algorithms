630f759172ed791e04c54e4d77c1088b05ecd0b6
utkarshmajumdar12 <93518648+utkarshmajumdar12@users.noreply.github.com>
2023-02-12 10:59:48 +0530

    Add files via upload

---
+++ LinkedList/insertattailLL.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+class node{
+  
+  public:
+  int data;
+  node* next;
+  
+  node(int val){
+      data = val;
+      next = NULL;
+  }
+};
+
+void insertAtbottom(node* &head, int val){
+    
+    node* n = new node(val);
+    node* temp = head;
+    
+    if(head == NULL){
+        head = n;
+        return;
+    }
+    
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->next =n;
+    
+
+}
+void display(node* head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<"";
+        temp = temp->next;
+    }
+}
+int main()
+{
+    
+
+    return 0;
+}
