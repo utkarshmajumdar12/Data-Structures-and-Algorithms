feb58f98c46caf4577d0452e619a71540350b81f
Utkarsh Majumdar <93518648+utkarshmajumdar12@users.noreply.github.com>
2023-05-24 22:26:45 +0530

    Create find-in-a-rotated-sorted-array-LC.cpp

---
+++ BinarySearch/find-in-a-rotated-sorted-array-LC.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+    while (low <= high) { 
+    int mid = (low + high) >> 1; 
+    if (nums[mid] == target)
+      return mid; 
+
+    if (nums[low] <= nums[mid]) { 
+      if (nums[low] <= target && nums[mid] >= target)
+        high = mid - 1; 
+      else
+        low = mid + 1; 
+    } else { 
+      if (nums[mid] <= target && target <= nums[high])
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+  }
+        return -1;
+        
+    }
+};
