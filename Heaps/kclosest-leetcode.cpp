d13bddd188a00215d6f610433634c549aab2ff4c
Utkarsh Majumdar <93518648+utkarshmajumdar12@users.noreply.github.com>
2023-06-03 22:50:01 +0530

    Create kclosest-leetcode.cpp

---
+++ Heaps/kclosest-leetcode.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+
+        priority_queue<pair<int,int>>maxh;
+        vector <int> res;
+        for(int i=0; i<arr.size();i++){
+            maxh.push({abs(arr[i]-x), arr[i]});
+            if(maxh.size()>k){
+                maxh.pop();
+            }
+        }
+            while(maxh.size()>0){
+                res.push_back(maxh.top().second);
+                maxh.pop();
+            }
+            sort(res.begin(), res.end());
+
+        return res;
+        
+        
+    }
+};
