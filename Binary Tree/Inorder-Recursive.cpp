f62a611dbe14a89c5968d07b57dc5c35cacab0cc
Utkarsh Majumdar <93518648+utkarshmajumdar12@users.noreply.github.com>
2023-07-20 10:42:25 +0530

    Create Inorder-Recursive.cpp

---
+++ Binary Tree/Inorder-Recursive.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+void helper(TreeNode* root,vector<int>& ans)
+    {
+        if(root==NULL)
+        {
+            return ;
+        }
+        helper(root->left,ans);
+        ans.push_back(root->val);
+        helper(root->right,ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        helper(root,ans);
+        return ans;
+            
+    }
+};
